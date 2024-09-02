28f97bdeb1f130a117963977bda93c3446f77693
Sandeep Kumaresan <sandeepkumaresan4676@gmail.com>
2024-09-02 21:51:25 +0530

    np array

--- 1st program.py
+++ 1st program.py
@@ -5,3 +5,7 @@ arr2 = np.arange(4, dtype = np.float64).reshape(2, 2)
 print("Array is of type: ", type(arr))
 print(arr1)
 print(arr2)
+print(arr2.ndim)# Printing array dimensions (axes)
+print(arr2.shape)# Printing shape of array
+print(arr2.size)# Printing size (total number of elements) of array
+print(arr2.dtype)# Printing type of elements in array
