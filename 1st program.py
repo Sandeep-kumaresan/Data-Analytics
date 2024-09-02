57e5e2a37668c2e3049bca9ab2fdf11cd695d340
Sandeep Kumaresan <sandeepkumaresan4676@gmail.com>
2024-09-02 17:59:40 +0530

    first commit

---
+++ 1st program.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr = np.array([[1,2,3],[4,5,6]])
+arr1 = np.arange(4,8,1.2)
+arr2 = np.arange(4, dtype = np.float64).reshape(2, 2)
+print("Array is of type: ", type(arr))
+print(arr1)
+print(arr2)
