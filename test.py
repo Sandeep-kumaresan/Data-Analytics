ab97ad7fa181fd713213d0590a9b2b1cdad92266
Sandeep Kumaresan <sandeepkumaresan4676@gmail.com>
2024-09-11 13:49:17 +0530

    check streamlit

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
-st.title("Working")
+st.title("Hi All!! Welcome")
 xpoints = np.array([0, 6])
 ypoints = np.array([0, 250])
 fig, ax = plt.subplots()
 ax.plot(xpoints, ypoints)
 st.pyplot(fig)
+st.code("print(x)")
 st.image("img/s.jpg")
+st.text("Download GTA VI for free!")
+st.download_button("Download GTA VI")
+st.warning("Your PC has been hacked!")
